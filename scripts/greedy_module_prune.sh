5acc391b54d315684b7043911f3780ff59523b0e
Francis Ferri <francisferrir@gmail.com>
2024-08-03 18:23:51 -0600

    Checkpoint : Pruning state update code ready

--- scripts/greedy_module_prune.sh
+++ scripts/greedy_module_prune.sh
@@ -5,9 +5,9 @@ source env/bin/activate
 #
 # Prune greedy
 #
-for task in "rte" "mrpc" "stsb" "cola" "sst2" "qnli" "qqp" "mnli"; do
-for model in "bert-base-uncased" "roberta-base"; do
-python3 prune.py --model_name_or_path=$model \
+for task in "rte"; do
+for model in "bert-base-uncased"; do
+python3 ./source/greedy_module_prune.py --model_name_or_path=$model \
     --task_name=$task \
     --seed=41 \
     --max_seq_length=128 \
@@ -15,13 +15,13 @@ python3 prune.py --model_name_or_path=$model \
     --learning_rate=2e-5 \
     --output_dir=experiments/tmp/ \
     --logging_dir=experiments/tmp/ \
-    --prune_n_layers=6 \
-    --prune_method="greedy" \
+    --prune_n_layers=4 \
+    --prune_method="greedy-module" \
     --overwrite_output_dir
 done
 done
 
-
+exit 1
 #
 # Prune optimal
 #
--- scripts/run_glue_layer_greedy.sh
+++ scripts/run_glue_layer_greedy.sh
@@ -3,32 +3,22 @@
 # Define arrays for model names, seeds, and task names
 models=("bert-base-uncased")
 seeds=(40 41 42 43 44)
-tasks=("mrpc")
+tasks=("mrpc" "rte" "stsb" "cola" "sst2" "qnli" "qqp" "mnli")
 prune_layers=(4)
 
-# Loop through all combinations
-for model in "${models[@]}"; do
-  for seed in "${seeds[@]}"; do
-    for task in "${tasks[@]}"; do
-      for layers in "${prune_layers[@]}"; do
-        output_dir="experiments/tmp/${model}_${task}_seed${seed}_prune${layers}"
-        
-        python3 ./source/run_glue.py \
-          --output_dir "$output_dir" \
-          --overwrite_output_dir \
-          --model_name_or_path "$model" \
-          --seed "$seed" \
-          --do_train \
-          --do_eval \
-          --max_seq_length 128 \
-          --per_device_train_batch_size 32 \
-          --num_train_epochs 3 \
-          --learning_rate 2e-5 \
-          --task_name "$task" \
-          --prune_n_layers "$layers" \
-          --prune_method "prune-greedy" \
-	 --job_id "prune-greedy-baselines" 
-      done
-    done
-  done
-done
+# Generate the combinations and run them in parallel
+parallel -j 2 -u python3 ./source/run_glue.py \
+  --output_dir experiments/tmp/{1}_{3}_seed{2}_prune{4} \
+  --overwrite_output_dir \
+  --model_name_or_path {1} \
+  --seed {2} \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --num_train_epochs 3 \
+  --learning_rate 2e-5 \
+  --task_name {3} \
+  --prune_n_layers {4} \
+  --prune_method prune-greedy \
+  --job_id prune-greedy-baselines ::: "${models[@]}" ::: "${seeds[@]}" ::: "${tasks[@]}" ::: "${prune_layers[@]}"
